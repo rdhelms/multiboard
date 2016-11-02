bf8fd944e44a0feb4f426e8ea556b624fc61ed3a
Brandon Fitch <fitchbj@gmail.com>
2016-11-02 17:21:06 -0400

    Changed file path again in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require ./node_modules/jquery/dist/jquery.min.js
+//= require ../node_modules/jquery/dist/jquery.min.js
 //= require angular/angular.min.js
 //= require angular-local-storage/dist/angular-local-storage.min.js
 //= require angular-ui-router/release/angular-ui-router.min.js
