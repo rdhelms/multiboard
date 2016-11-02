2e053e0a7f9d2f6e25baebc7641e0048553ec0b7
Robby Helms <robert.helms1@gmail.com>
2016-11-02 16:10:50 -0400

    Styled canvas and added buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102181121) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 end
