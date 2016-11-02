bdf87d42f34ad24688efea796c0a45beffb19203
Brandon Fitch <fitchbj@gmail.com>
2016-11-02 17:26:11 -0400

    Another adjustment to find the right file path.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require jquery.min.js
+//= require jquery/jquery.min.js
 //= require angular/angular.min.js
 //= require angular-local-storage/dist/angular-local-storage.min.js
 //= require angular-ui-router/release/angular-ui-router.min.js
