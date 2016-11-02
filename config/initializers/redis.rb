5095e2ae3484271ea6f44475fa4c0ca35d4b4c1a
Brandon Fitch <fitchbj@gmail.com>
2016-11-02 17:13:51 -0400

    Heroku try 1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,9 +12,9 @@ require 'redis'
 # # To clear out the db before each test
 # Redis.current.flushdb if Rails.env == "test"
 # Redis.current = Redis.new(:host => 'localhost', :port => 6379)
-if Rails.env.production?
-  uri = URI.parse(ENV['REDIS_URL'])
-  Redis.current = Redis.new(:url => uri)
-else
-  Redis.current = Redis.new(:host => 'localhost', :port => 6379)
-end
+# if Rails.env.production?
+#   uri = URI.parse(ENV['REDIS_URL'])
+#   Redis.current = Redis.new(:url => uri)
+# else
+#   Redis.current = Redis.new(:host => 'localhost', :port => 6379)
+# end
